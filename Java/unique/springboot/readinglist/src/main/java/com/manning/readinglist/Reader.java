e4ff0ce84471ba8e207e2f69f97dfde699414ff3
Harlin Seritt <harlin.seritt@gmail.com>
2016-06-20 19:16:47 -0600

    Added Chapter 3 for Spring Boot

---
+++ Java/unique/springboot/readinglist/src/main/java/com/manning/readinglist/Reader.java
@@ -0,0 +1,77 @@
+package com.manning.readinglist;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+public class Reader implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String username;
+	private String password;
+	private String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("READER"));
+	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
--- Java/unique/springboot/readinglist/src/main/java/com/manning/readinglist/ReadingListController.java
+++ Java/unique/springboot/readinglist/src/main/java/com/manning/readinglist/ReadingListController.java
@@ -3,6 +3,7 @@ package com.manning.readinglist;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon")
 public class ReadingListController {
+	private String associateId;
 	private ReadingListRepository readingListRepository;
 	
 	@Autowired
@@ -25,8 +28,14 @@ public class ReadingListController {
 		List<Book> readingList = readingListRepository.findByReader(reader);
 		if (readingList != null) {
 			model.addAttribute("books", readingList);
+			model.addAttribute("reader", reader);
+			model.addAttribute("amazonID", associateId);
 		}
 		return "readingList";
+	}
+	
+	public void setAssociateId(String associateId) {
+		this.associateId = associateId;
 	}
 	
 	@RequestMapping(value="/{reader}", method=RequestMethod.POST)
